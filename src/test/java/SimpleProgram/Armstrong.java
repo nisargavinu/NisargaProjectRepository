62d9ab1bc7ef60352eba593e004c29e078bc6994
nisargavinu <nisargakt139@gmail.com>
2024-01-17 19:45:42 +0530

    added armstorg projects

--- src/test/java/SimpleProgram/Armstrong.java
+++ src/test/java/SimpleProgram/Armstrong.java
@@ -1,4 +1,28 @@
 package SimpleProgram;
+import java.lang.*;
+import java.io.*;
 
 public class Armstrong {
+
+    public void findArm(int num){
+        int rem, sum=0, num1=num;
+        while(num1>0) {
+            rem = num1 % 10;
+            num1= num1 / 10;
+            sum = sum + (rem * rem * rem);
+        }
+        if(sum==num){
+            System.out.println("Provided number " +num+ "Armstrong number");
+        }
+        else{
+            System.out.println("Provided number " +num+ "is not Armstrong number");
+        }
+
+    }
+
+    public static void main(String[] arg){
+        Armstrong am = new Armstrong();
+        am.findArm(153);
+
+    }
 }
